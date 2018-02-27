7445c918919e8244742b9f1634b8e7ee55a027e2
Ilya Sysoi <14ilya12322@gmail.com>
2018-02-27 18:48:29 +0300

    add javascript file

---
+++ UI/javascript.js
@@ -0,0 +1,81 @@
+var photoPosts = [
+    {
+        id: '1',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '2',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '3',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '4',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '5',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '6',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    },
+    {
+        id: '7',
+        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
+        createdAt: new Date('2018-02-23T23:00:00'),
+        author: 'Иванов Иван',
+        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
+    }
+
+];
+
+function getPhotoPosts(skip, top, filterConfig)  {
+    return photoPosts
+}
+
+function getPhotoPost(id) {
+    photoPosts.forEach(function(photoPost) {
+        if (photoPost.id === id) {
+            return photoPost
+        }
+    });
+}
+
+function validatePhotoPost(photoPost) {
+    return !!(photoPosts.id, photoPost.descriprion, photoPost.createdAt, photoPost.author, photoPost.photoLink);
+}
+
+
+function addPhotoPost(photoPost) {
+    if (validatePhotoPost(photoPost)) {
+        photoPosts.append(photoPost)
+    }
+}
+
+
+function editPhotoPost(id, photoPost) {
+    var post = getPhotoPost(id)
+    post
+}
