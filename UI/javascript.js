5944558120cda97a581c9ecced91ab52e36aaf93
Ilya Sysoi <14ilya12322@gmail.com>
2018-03-04 23:46:27 +0300

    update js file

--- UI/javascript.js
+++ UI/javascript.js
@@ -1,81 +1,140 @@
-var photoPosts = [
-    {
-        id: '1',
-        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-    },
-    {
-        id: '2',
-        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-    },
-    {
-        id: '3',
-        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-    },
-    {
-        id: '4',
-        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-    },
-    {
-        id: '5',
-        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-    },
-    {
-        id: '6',
-        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-    },
-    {
-        id: '7',
-        descriprion: 'Женская сборная Беларуси выиграла эстафету в рамках соревнований по биатлону на Олимпийских играх в Пхёнчхане!!!',
-        createdAt: new Date('2018-02-23T23:00:00'),
-        author: 'Иванов Иван',
-        photoLink: 'http://ont.by/webroot/delivery/files/news/2018/02/22/Dom.jpg'
-    }
+Date.prototype.yyyymmdd = function() {
+    var mm = this.getMonth() + 1; // getMonth() is zero-based
+    var dd = this.getDate();
 
-];
+    return [(dd>9 ? '' : '0') + dd,
+        (mm>9 ? '' : '0') + mm,
+        this.getFullYear()
+    ].join('.');
+};
 
-function getPhotoPosts(skip, top, filterConfig)  {
-    return photoPosts
-}
+(function() {
 
-function getPhotoPost(id) {
-    photoPosts.forEach(function(photoPost) {
-        if (photoPost.id === id) {
-            return photoPost
+    var photoPosts = [
+        createPost(0, "Test post 1", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(1, "Test post 2", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(2, "Test post 3", new Date('February 17, 2018 15:24:00'), "Alex", "photo1.jpg"),
+        createPost(3, "Test post 4", new Date('February 18, 2018 13:24:00'), "Vasya", "photo1.jpg"),
+        createPost(4, "Test post 5", new Date('February 17, 2018 13:24:00'), "Ilada", "photo1.jpg"),
+        createPost(5, "Test post 6", new Date('February 17, 2018 17:24:00'), "Zheka", "photo1.jpg"),
+        createPost(6, "Test post 7", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(7, "Test post 8", new Date('February 17, 2018 17:24:00'), "Yana", "photo1.jpg"),
+        createPost(8, "Test post 9", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(9, "Test post 10", new Date('February 17, 2018 17:24:00'), "Zheka", "photo1.jpg"),
+        createPost(10, "Test post 11", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(11, "Test post 12", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(12, "Test post 13", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(13, "Test post 14", new Date('February 17, 2018 12:24:00'), "san", "photo1.jpg"),
+        createPost(14, "Test post 15", new Date('February 18, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(15, "Test post 16", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(16, "Test post 17", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(17, "Test post 18", new Date('February 19, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(18, "Test post 19", new Date('February 17, 2018 17:24:00'), "Mark", "photo1.jpg"),
+        createPost(19, "Test post 20", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg")
+    ];
+
+    function createPost(id, description, createdAt, author, photoLink) {
+        return {
+            id: id,
+            description: description,
+            createdAt: createdAt,
+            author: author,
+            photoLink: photoLink
+
         }
-    });
-}
-
-function validatePhotoPost(photoPost) {
-    return !!(photoPosts.id, photoPost.descriprion, photoPost.createdAt, photoPost.author, photoPost.photoLink);
-}
-
-
-function addPhotoPost(photoPost) {
-    if (validatePhotoPost(photoPost)) {
-        photoPosts.append(photoPost)
     }
-}
+
+    function filter(date, author) {
+        this.date = date;
+        this.author = author;
+    }
+
+    function getPhotoPosts(skip, top, filterConfig) {
+        return photoPosts.slice(skip, skip + top);
+    }
+
+    function getPhotoPost(id){
+        for (var i = 0; i < photoPosts.length; i++) {
+            if(photoPosts[i].id === id)
+                return photoPosts[i];
+        }
+        return null;
+    }
+
+    function validatePhotoPost(photoPost) {
+        return photoPost.description !== undefined
+            && photoPost.description.length <= 200
+            && photoPost.createdAt !== undefined
+            && photoPost.author !== undefined
+            && photoPost.photoLink !== undefined;
+
+    }
+
+    function addPhotoPost(photoPost) {
+        if(validatePhotoPost(photoPost)) {
+            photoPost.id = photoPosts.length;
+            photoPosts.push(photoPost);
+            return true;
+        }
+        return false;
+    }
+
+    function findIndexOfPhotoPost(id) {
+        for (var i = 0; i < posts.length; i++) {
+            if(photoPosts[i].id === id)
+                return i;
+        }
+        return -1;
+    }
+
+    function editPhotoPost(id, photoPost) {
+        var post = getPhotoPost(id);
+        if(post != null)
+        {
+            if(photoPost.photoLink !== undefined)
+                post.photoLink = photoPost.photoLink;
+            if(photoPost.description !== undefined)
+                post.description = photoPost.description;
+            if(validatePhotoPost(photoPost))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function removePhotoPost(id) {
+        if(getPhotoPost(id))
+        {
+            var index = findIndexOfPhotoPost(id);
+            if(index !== -1)
+            {
+                photoPosts.splice(index, 1);
+                return true;
+            }
+        }
+        return false;
+    }
 
 
-function editPhotoPost(id, photoPost) {
-    var post = getPhotoPost(id)
-    post
-}
+    var posts = getPhotoPosts(0, 10);
+
+    for (var i = 0; i < posts.length; i++) {
+        var html = "<div class=\"Post\"> \
+                        <div class=\"editButton\"> \
+                            <button><img src=\"assets/pencil-edit-button.png\" height=32 width=32/>  </button> \
+                        </div> \
+                        <img class=\"photoPost\" src=\"assets/photo1.png\" height=\"80%\" width=\"60%\"/> \
+                        <div class=\"description\"> \
+                            <p>"+ posts[i].author +"</p> \
+                            <p>"+ posts[i].createdAt.getUTCHours() + ":" + posts[i].createdAt.getUTCMinutes() + " " + posts[i].createdAt.yyyymmdd() +"</p> \
+                            <p>"+ posts[i].description +"</p> \
+                        </div> \
+                        <button class=\"button\" style=\"top: 50px; left: 40px;\">Like 54</button> \
+                        <button class=\"button\" style=\"top: 50px; left: 50%\">Удалить</button> \
+                        <div class=\"border\"></div> \
+                    </div>";
+        document.write(html);
+    }
+})();
+
