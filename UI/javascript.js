b57e05f2b6e29933bef8b7cbcd11a9992b52988a
Ilya Sysoi <14ilya12322@gmail.com>
2018-03-05 00:07:57 +0300

    update js file

--- UI/javascript.js
+++ UI/javascript.js
@@ -11,25 +11,25 @@ Date.prototype.yyyymmdd = function() {
 (function() {
 
     var photoPosts = [
-        createPost(0, "Test post 1", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(1, "Test post 2", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(2, "Test post 3", new Date('February 17, 2018 15:24:00'), "Alex", "photo1.jpg"),
+        createPost(0, "Test post 1", new Date('February 12, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(1, "Test post 2", new Date('February 17, 2018 21:24:00'), "Vasya", "photo1.jpg"),
+        createPost(2, "Test post 3", new Date('February 17, 2018 15:42:00'), "Alex", "photo1.jpg"),
         createPost(3, "Test post 4", new Date('February 18, 2018 13:24:00'), "Vasya", "photo1.jpg"),
-        createPost(4, "Test post 5", new Date('February 17, 2018 13:24:00'), "Ilada", "photo1.jpg"),
-        createPost(5, "Test post 6", new Date('February 17, 2018 17:24:00'), "Zheka", "photo1.jpg"),
-        createPost(6, "Test post 7", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(4, "Test post 5", new Date('February 17, 2018 20:53:00'), "Ilada", "photo1.jpg"),
+        createPost(5, "Test post 6", new Date('February 17, 2018 11:44:00'), "Zheka", "photo1.jpg"),
+        createPost(6, "Test post 7", new Date('February 17, 2018 16:24:00'), "Vasya", "photo1.jpg"),
         createPost(7, "Test post 8", new Date('February 17, 2018 17:24:00'), "Yana", "photo1.jpg"),
-        createPost(8, "Test post 9", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(9, "Test post 10", new Date('February 17, 2018 17:24:00'), "Zheka", "photo1.jpg"),
+        createPost(8, "Test post 9", new Date('February 17, 2018 17:18:00'), "Vasya", "photo1.jpg"),
+        createPost(9, "Test post 10", new Date('February 17, 2018 17:54:00'), "Zheka", "photo1.jpg"),
         createPost(10, "Test post 11", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(11, "Test post 12", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(12, "Test post 13", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(13, "Test post 14", new Date('February 17, 2018 12:24:00'), "san", "photo1.jpg"),
+        createPost(11, "Test post 12", new Date('February 17, 2018 17:04:00'), "Vasya", "photo1.jpg"),
+        createPost(12, "Test post 13", new Date('February 17, 2018 19:24:00'), "Vasya", "photo1.jpg"),
+        createPost(13, "Test post 14", new Date('February 17, 2018 12:27:00'), "san", "photo1.jpg"),
         createPost(14, "Test post 15", new Date('February 18, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(15, "Test post 16", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
+        createPost(15, "Test post 16", new Date('February 17, 2018 9:15:00'), "Vasya", "photo1.jpg"),
         createPost(16, "Test post 17", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(17, "Test post 18", new Date('February 19, 2018 17:24:00'), "Vasya", "photo1.jpg"),
-        createPost(18, "Test post 19", new Date('February 17, 2018 17:24:00'), "Mark", "photo1.jpg"),
+        createPost(17, "Test post 18", new Date('February 19, 2018 10:24:00'), "Vasya", "photo1.jpg"),
+        createPost(18, "Test post 19", new Date('February 17, 2018 13:54:00'), "Mark", "photo1.jpg"),
         createPost(19, "Test post 20", new Date('February 17, 2018 17:24:00'), "Vasya", "photo1.jpg")
     ];
 
@@ -44,13 +44,21 @@ Date.prototype.yyyymmdd = function() {
         }
     }
 
-    function filter(date, author) {
-        this.date = date;
-        this.author = author;
-    }
 
     function getPhotoPosts(skip, top, filterConfig) {
-        return photoPosts.slice(skip, skip + top);
+        if(filterConfig === undefined ){
+            return photoPosts.slice(skip, skip + top);
+        }
+        else {
+            if (filterConfig.author !== undefined
+                && filterConfig.author !== "") {
+                return photoPosts.filter(function(author) {
+                    return author === filterConfig.author;
+                })
+            }
+
+        }
+
     }
 
     function getPhotoPost(id){
